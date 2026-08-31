959ddfe960ac3aaada630e713366369655d0ca04
Afia <assafowaa001@st.ug.edu.gh>
2026-08-31 20:54:09 +0000

    Add IMovable interface with Car and Bicycle implementations

---
+++ dcit318-assignment2-22024353/IMovable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
