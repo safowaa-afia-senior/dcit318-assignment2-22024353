08079ef63c1a5093f8d8711cf4128833ad518f55
Afia <assafowaa001@st.ug.edu.gh>
2026-09-01 00:27:22 +0000

    Update Program.cs to demonstrate all tasks

--- dcit318-assignment2-22024353/Program.cs
+++ dcit318-assignment2-22024353/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Task 1: Inheritance and Method Overriding
+        Console.WriteLine("Task 1: Inheritance and Method Overriding");
+
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        animal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+
+        Console.WriteLine();
+
+        // Task 2: Abstract Classes and Methods
+        Console.WriteLine("Task 2: Abstract Classes and Methods");
+
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+        Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+        Console.WriteLine();
+
+        // Task 3: Interfaces
+        Console.WriteLine("Task 3: Interfaces");
+
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
